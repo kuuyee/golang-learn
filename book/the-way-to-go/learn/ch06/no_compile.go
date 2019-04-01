5bfa3609125d90e4ab6b73962e4f570da4780ed6
yeekuu <kuuyee@gmail.com>
2019-04-02 01:11:50 +0800

    完成6.2章节部分练习

---
+++ book/the-way-to-go/learn/ch06/no_compile.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	
+}
+
+
+func Pop(st [4]int) int {
+	v := 0
+	for ix := len(st) - 1; ix >= 0; ix-- {
+		if v = st[ix]; v != 0 {
+			st[ix] = 0
+			return v
+		}
+		return 3
+	}
+	return 5
+}
+
---
+++ book/the-way-to-go/learn/ch06/simple_funcation.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Multiply 2*5*6 = %d\n",MultiPly3Nums(2,5,6))
+	
+}
+
+func MultiPly3Nums(a int,b int,c int)  int {
+	return a*b*c
+}
